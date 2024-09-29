8c2e6f99fbc75de226beea91c5acaaa1aa3a5825
Oleg Barenboim <chessbyte@gmail.com>
2024-09-29 11:41:19 -0400

    add ~/.chezmoi.sh

---
+++ dot_chezmoi.sh
@@ -0,0 +1,14 @@
+# Install Chezmoi (https://www.chezmoi.io/), if needed
+brew list chezmoi &> /dev/null || brew install chezmoi
+
+# Ensure Chezmoi is installed
+command -v chezmoi &> /dev/null || {
+  echo "chezmoi not found - please install it"
+  return 1 || exit 1
+}
+
+# Initialize Chezmoi
+[ -d ~/.local/share/chezmoi/.git ] || chezmoi init https://github.com/chessbyte/dotfiles.git
+
+# Update the dotfiles via Chezmoi
+chezmoi update
