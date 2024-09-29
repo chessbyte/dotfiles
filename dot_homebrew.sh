09dedc8c743338b95b4a974704acd7988e5f4c26
Oleg Barenboim <chessbyte@gmail.com>
2024-09-29 11:42:38 -0400

    [~/.homebrew.sh] tweak auto-install

--- dot_homebrew.sh
+++ dot_homebrew.sh
@@ -1,2 +1,8 @@
-[ ! -f /opt/homebrew/bin/brew ] && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install Homebrew (https://brew.sh/), if needed
+[ -f /opt/homebrew/bin/brew ] || {
+  echo "Homebrew not found - installing it now"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+# Initialize Homebrew
 [ -f /opt/homebrew/bin/brew ] && eval "$(/opt/homebrew/bin/brew shellenv)"
